2c54411bde48fbc4d0e8cb8b5cdb46d61d51d965
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-03-03 22:30:35 +0100

    fixing bug

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -19,14 +19,14 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
       const updateIngredient = { [ action.ingredientName ]: state.ingredients[ action.ingredientName ] + 1 }
-      const updateIngredients = updateObject(state.ingredientName, updateIngredient)
+      const updateIngredients = updateObject(state.ingredients, updateIngredient)
       return updateObject(state, {
         ingredients: updateIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[ action.ingredientName ]
       })
     case actionTypes.REMOVE_INGREDIENT:
       const updateIg = { [ action.ingredientName ]: state.ingredients[ action.ingredientName ] - 1 }
-      const updateIgs = updateObject(state.ingredientName, updateIg)
+      const updateIgs = updateObject(state.ingredients, updateIg)
       return updateObject(state, {
         ingredients: updateIgs,
         totalPrice: state.totalPrice - INGREDIENT_PRICES[ action.ingredientName ]
