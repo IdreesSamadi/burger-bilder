0a6c3618650125466d862affa8fc22fc4b099f6d
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-04-21 20:18:54 +0200

    fixing bug with routes in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import { Route, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import Logout from './containers/Auth/Logout/Logout'
 import { connect } from 'react-redux'
 import { authCheckState } from './store/actions/index'
@@ -17,36 +17,34 @@ const asyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth')
 })
 
-//TODO: fix the bug with routes it causes memory leak
-
 class App extends Component {
   componentDidMount() {
     this.props.onTryAuthSignup()
   }
   render() {
-    //   let routes = (
-    //     <Switch>
-    //       <Route path='/' exact component={ BurgerBuilder } />
-    //       <Route path='/auth' component={ Auth } />
-    //     </Switch>
-    //   )
-    //   if(this.props.isAuthenticate) {
-    //     routes = (
-    //       <Switch>
-    //         <Route path='/orders' component={ Order } />
-    //         <Route path='/checkout' component={ Checkout } />
-    //         <Route path='/logout' component={ Logout } />
-    //       </Switch>
-    //     )
-    //   }
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={ asyncAuth } />
+        <Route path='/' exact component={ BurgerBuilder } />
+        <Redirect to="/" />
+      </Switch>
+    )
+    if (this.props.isAuthenticate) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={ asyncCheckout } />
+          <Route path='/orders' component={ asyncOrder } />
+          <Route path='/logout' component={ Logout } />
+          <Route path='/auth' component={ asyncAuth } />
+          <Route path='/' exact component={ BurgerBuilder } />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Route path='/orders' component={ asyncOrder } />
-          <Route path='/' exact component={ BurgerBuilder } />
-          <Route path='/checkout' component={ asyncCheckout } />
-          <Route path='/auth' component={ asyncAuth } />
-          <Route path='/logout' component={ Logout } />
+          { routes }
         </Layout>
       </div>
     )
